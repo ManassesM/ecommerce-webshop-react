e5a96c55aef72a679d711747bd0d5879ebdfaf4d
ManassesM <manamiui2001@gmail.com>
2021-12-01 21:57:13 -0300

    setup app layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from './Header'
+import Footer from './Footer'
+import Routes from '../routes/Routes'
+
 const Layout = () => {
-  return (
-    <div>
-      Layout Component
-    </div>
-  )
+	return (
+		<BrowserRouter>
+			<Route
+				render={(props) => (
+					<div>
+						<div className='container'>
+							<Header {...props} />
+							<div className='main'>
+								<Routes />
+							</div>
+						</div>
+						<Footer />
+					</div>
+				)}
+			></Route>
+		</BrowserRouter>
+	)
 }
 
 export default Layout
