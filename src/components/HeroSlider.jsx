ef17c3a1fe993c25c6c69f5cd33911105a61b1f4
ManassesM <manamiui2001@gmail.com>
2021-12-07 14:04:17 -0300

    make button component

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import Button from './Button'
 
 const HeroSlider = props => {
   
@@ -45,20 +46,20 @@ const HeroSlider = props => {
       }
       {
         props.control ? (
-          <div className="hero-slider__control">
+          <div className='hero-slider__control'>
             
-            <div className="hero-slider__control__item" onClick={prevSlide}>
-              <i className="bx bx-chevron-left"></i>
+            <div className='hero-slider__control__item' onClick={prevSlide}>
+              <i className='bx bx-chevron-left'></i>
             </div>
 
-            <div className="hero-slider__control__item">
-              <div className="index">
+            <div className='hero-slider__control__item'>
+              <div className='index'>
                 {activeSlide + 1}/{data.length}
               </div>
             </div>
 
-            <div className="hero-slider__control__item" onClick={nextSlide}>
-              <i className="bx bx-chevron-right"></i>
+            <div className='hero-slider__control__item' onClick={nextSlide}>
+              <i className='bx bx-chevron-right'></i>
             </div>
 
           </div>
@@ -78,31 +79,32 @@ HeroSlider.propTypes = {
 const HeroSliderItem = props => (
   <div className={`hero-slider__item ${props.active && 'active'}`}>
       
-      <div className="hero-slider__item__info">
+      <div className='hero-slider__item__info'>
           <div className={`hero-slider__item__info__title color-${props.item.color}`}>
               <span>{props.item.title}</span>
           </div>
           
-          <div className="hero-slider__item__info__description">
+          <div className='hero-slider__item__info__description'>
               <span>{props.item.description}</span>
           </div>
           
-          <div className="hero-slider__item__info__btn">
+          <div className='hero-slider__item__info__btn'>
               <Link to={props.item.path}>
-                  <button
-                      // backgroundColor={props.item.color}
-                      icon="bx bx-cart"
-                      // animate={true}
+                  <Button
+                      backgroundColor={props.item.color}
+                      icon='bx bx-cart'
+                      animate={true}
+                      size='sm'
                   >
                     More Details
-                  </button>
+                  </Button>
               </Link>
           </div>
       </div>
 
-      <div className="hero-slider__item__image">
+      <div className='hero-slider__item__image'>
           <div className={`shape bg-${props.item.color}`}></div>
-          <img src={props.item.img} alt="" />
+          <img src={props.item.img} alt='' />
       </div>
 
   </div>
