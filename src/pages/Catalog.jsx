d9926a4b779325930ce27ef1464cffad5708abb4
ManassesM <manamiui2001@gmail.com>
2021-12-09 10:26:07 -0300

    make checkbox

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+
 import Helmet from '../components/Helmet'
 import Grid from '../components/Grid'
 import ProductCard from '../components/ProductCard'
+import CheckBox from '../components/CheckBox'
 
 import productData from '../assets/fake-data/products'
+import category from '../assets/fake-data/category'
+import colors from '../assets/fake-data/product-color'
+import size from '../assets/fake-data/product-size'
+import Button from '../components/Button'
 
 const Catalog = () => {
   return (
@@ -11,7 +17,70 @@ const Catalog = () => {
       <div className="catalog">
         
         <div className="catalog__filter">
-          
+   
+          {/* catalog */}
+          <div className="catalog__filter__widget"> 
+            <div className="catalog__filter__widget__title">
+              Catalog 
+            </div>
+            
+            <div className="catalog__filter__widget__content">
+              {
+                category.map((item, idx) => (
+                  <div key={idx} className='catalog__filter__widget__content__item'>
+                    <CheckBox
+                      label={item.display}
+                    />
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+
+          {/* colors */}
+          <div className="catalog__filter__widget">  
+            <div className="catalog__filter__widget__title">
+              Color
+            </div>
+            
+            <div className="catalog__filter__widget__content">
+              {
+                colors.map((item, idx) => (
+                  <div key={idx} className='catalog__filter__widget__content__item'>
+                    <CheckBox
+                      label={item.display}
+                    />
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+
+          {/* sizes */}
+          <div className="catalog__filter__widget">  
+            <div className="catalog__filter__widget__title">
+              Size
+            </div>
+            
+            <div className="catalog__filter__widget__content">
+              {
+                size.map((item, idx) => (
+                  <div key={idx} className='catalog__filter__widget__content__item'>
+                    <CheckBox
+                      label={item.display}
+                    />
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+
+          <div className="catalog__filter__widget">
+            <div className="catalog__filter_widget__content">
+              <Button size='sm' >Remove filter</Button>
+            </div>
+          </div>
+
         </div>
         
         <div className="catalog__content">
@@ -27,7 +96,7 @@ const Catalog = () => {
                   key={idx}
                   img01={item.image01}
                   img02={item.image02}
-                  name={item.name}
+                  name={item.title}
                   price={Number(item.price)}
                   slug={item.slug}
                 >
