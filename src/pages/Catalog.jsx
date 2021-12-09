4b1203dab14b714bc03cb009060b1d994f46fe06
ManassesM <manamiui2001@gmail.com>
2021-12-09 09:58:55 -0300

    fix commit

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import Helmet from '../components/Helmet'
+import Grid from '../components/Grid'
+import ProductCard from '../components/ProductCard'
 
-
+import productData from '../assets/fake-data/products'
 
 const Catalog = () => {
   return (
     <Helmet title='Catalog'>
-      Catalog
+      <div className="catalog">
+        
+        <div className="catalog__filter">
+          
+        </div>
+        
+        <div className="catalog__content">
+          <Grid
+            col={3}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            {
+              productData.getAllProducts().map((item, idx) => (
+                <ProductCard 
+                  key={idx}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.name}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                >
+                  
+                </ProductCard>
+              ))
+            }
+          </Grid>
+        </div>
+
+      </div>
     </Helmet>
   )
 }
