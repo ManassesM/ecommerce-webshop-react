d5eeb883d3615950076fc96a59ff4affc03c4fab
ManassesM <manamiui2001@gmail.com>
2021-12-09 09:27:04 -0300

    make product card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,12 @@ import HeroSlider from '../components/HeroSlider'
 import Section, { SectionTitle, SectionBody } from '../components/Section'
 import PolicyCard from '../components/PolicyCard'
 import Grid from '../components/Grid'
+import ProductCard from '../components/ProductCard'
 
 import heroSliderData from '../assets/fake-data/hero-slider'
 import policy from '../assets/fake-data/policy'
+import productData from '../assets/fake-data/products'
+
 import { Link } from 'react-router-dom'
 
 const Home = () => {
@@ -47,6 +50,42 @@ const Home = () => {
           </Grid>
         </SectionBody>
       </Section>
+      {/* end policy section */}
+
+      {/* best selling section */}
+      <Section>
+        
+        <SectionTitle>
+          Top selling products of the week 
+        </SectionTitle>
+          
+        <SectionBody>
+        <Grid
+            col={4}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            
+            {
+              productData.getProducts(4).map((item, idx) => (
+                <ProductCard
+                  key={idx}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+
+                />
+              ))
+            }
+
+          </Grid>
+        </SectionBody>
+
+      </Section>
+      {/* end selling section */}
 
 		</Helmet>
 	)
